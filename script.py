792bab3f36a782e45d828e673d233a664f7c578b
David Gutiérrez <david.gutierrez@estudiants.urv.cat>
2020-12-10 19:57:43 +0100

    first commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import os
+import schedule
+import time
+import sys
+x = 1
+
+def job():
+    global x
+    print("Speedtest at ", sys.argv[x])
+    os.system("speedtest")
+    x += 1
+
+for arg in sys.argv:
+    if arg != "script.py":
+        schedule.every().day.at(arg).do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
